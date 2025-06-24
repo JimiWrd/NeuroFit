6aeefec2fd4e17a68a36586454bd60a5234d339b
Jimi <jimi.ward@dizplai.com>
2025-06-25 00:10:20 +0100

    add eureka server basic config and tests

--- infrastructure/eureka-server/src/test/java/io/github/jimiwrd/eurekaserver/EurekaServerApplicationTests.java
+++ infrastructure/eureka-server/src/test/java/io/github/jimiwrd/eurekaserver/EurekaServerApplicationTests.java
@@ -2,12 +2,28 @@ package io.github.jimiwrd.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class EurekaServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@LocalServerPort
+	private int port;
 
+	@Test
+	void contextLoads() {}
+
+	@Test
+	void eurekaServerIsUp() {
+		String url = String.format("http://localhost:%d/eureka/apps", port);
+		ResponseEntity<String> response = new RestTemplate()
+				.getForEntity(url, String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
