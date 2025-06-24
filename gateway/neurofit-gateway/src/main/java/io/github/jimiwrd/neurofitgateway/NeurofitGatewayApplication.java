0d07fa368664ddf37470553561c14081f427e902
Jimi <jimi.ward@dizplai.com>
2025-06-24 23:12:09 +0100

    add neurofit-gateway service to gateway dir

---
+++ gateway/neurofit-gateway/src/main/java/io/github/jimiwrd/neurofitgateway/NeurofitGatewayApplication.java
@@ -0,0 +1,13 @@
+package io.github.jimiwrd.neurofitgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeurofitGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeurofitGatewayApplication.class, args);
+	}
+
+}
